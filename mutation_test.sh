93b5f1448750826faa7b59f6c2ca601088f01e4e
Samuel Veloso Marçal <svm@lionx.com.br>
2022-07-22 07:24:35 -0300

    chore: updated mutatest requirements and bash files

--- mutation_test.sh
+++ mutation_test.sh
@@ -1,1 +1,2 @@
-mutatest -s caronte/src -y 'if' 'nc' 'ix' 'su' 'bs' 'bc' 'bn' -x 60 -n 1000 -t 'python3 -m pytest'
+pip install -r requirements-dev.txt
+mutatest -s caronte/src -y 'if' 'nc' 'ix' 'su' 'bs' 'bc' 'bn' -x 60 -n 1000 -t 'python -m pytest'
