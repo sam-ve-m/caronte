dacd650ab489457964076f43013681abd2b69358
Vinicius Reis <vinicius.reis@lionx.com.br>
2022-09-08 18:32:13 -0300

    fix: map-response when expired token

--- caronte/src/domain/enums/response.py
+++ caronte/src/domain/enums/response.py
@@ -4,7 +4,7 @@ from enum import Enum
 
 class CaronteStatus(Enum):
     SUCCESS = "success"
-    FORBIDDEN = "unauthorized_token"
+    BAD_REQUEST = "unauthorized_token"
     TOKEN_NOT_FOUND = "token_not_found"
     UNEXPECTED_ERROR = "unexpected_error_has_occurred"
 
